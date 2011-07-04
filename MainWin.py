735138249fd1244e1f77168ae6481b6ff05c171d
Edward Sitarski <edward.sitarski@gmail.com>
2011-07-04 07:59:44 -0400

    Fixed problems with importing categories.

--- MainWin.py
+++ MainWin.py
@@ -545,7 +545,7 @@ class MainWin( wx.Frame ):
 			except IOError:
 				Utils.MessageOK( self, "Cannot open file:\n%s" % categoriesFile, "File Open Error", iconMask=wx.ICON_ERROR)
 			except (ValueError, IndexError):
-				Utils.MessageOK( self, "Bad file format:\n%s" % categoriesFile, "File Read Error", iconMask=wx.ICON_ERROR)
+				Utils.MessageOK( self, "Bad file format:\n%s\n\n%s - %s" % (categoriesFile, str(ValueError), str(IndexError)), "File Read Error", iconMask=wx.ICON_ERROR)
 
 		if not importedCategories:
 			race.categories = categoriesSave
