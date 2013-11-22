c48f90231706d56f77d17e5fe41149cccf26092d
Edward Sitarski <edward.sitarski@gmail.com>
2013-11-22 10:24:52 -0500

    Fixed problem with overriden logging message.

--- LogPrintStackStderr.py
+++ LogPrintStackStderr.py
@@ -4,38 +4,38 @@ import datetime
 import traceback
 
 class LogPrintStackStderr( wx.PyLog ):
-	def logHeader( self ):
-		sys.stderr.write( '*' * 78 + '\n' )
-		sys.stderr.write( '* wxPython Exception: {}\n'.format( datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') ) )
-		sys.stderr.write( '*' * 78 + '\n' )
+    def logHeader( self ):
+        sys.stderr.write( '*' * 78 + '\n' )
+        sys.stderr.write( '* wxPython Exception: {}\n'.format( datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') ) )
+        sys.stderr.write( '*' * 78 + '\n' )
 
-	def doPrint( self, *args, **kwargs ):
-		sys.stderr.write( u': '.join(u'{}'.format(a) for a in args) )
-		sys.stderr.write( '\n' )
-		for k, v in kwargs.iteritems():
-			sys.stderr.write( u'{}: {}\n'.format(k,v) )
-				
-	def DoLogText( self, *args, **kwargs ):
-		self.logHeader()
-		traceback.print_stack( file=sys.stderr )
-		self.doPrint( *args, **kwargs )
+    def doPrint( self, *args, **kwargs ):
+        sys.stderr.write( u': '.join(u'{}'.format(a) for a in args) )
+        sys.stderr.write( '\n' )
+        for k, v in kwargs.iteritems():
+            sys.stderr.write( u'{}: {}\n'.format(k,v) )
+                
+    def DoLogText( self, *args, **kwargs ):
+        self.logHeader()
+        traceback.print_stack( file=sys.stderr )
+        self.doPrint( *args, **kwargs )
 
-	def DoLogString( self, *args, **kwargs ):
-		self.logHeader()
-		traceback.print_stack( file=sys.stderr )
-		self.doPrint( *args, **kwargs )
-	
-	def DoLogRecord( self, *args, **kwargs ):
-		self.logHeader()
-		traceback.print_stack( file=sys.stderr )
-		self.doPrint( *args, **kwargs )
-		
-	def DoLogTextAtLevel( self, *args, **kwargs ):
-		self.logHeader()
-		traceback.print_stack( file=sys.stderr )
-		self.doPrint( *args, **kwargs )
-		
+    def DoLogString( self, *args, **kwargs ):
+        self.logHeader()
+        traceback.print_stack( file=sys.stderr )
+        self.doPrint( *args, **kwargs )
+    
+    def DoLogRecord( self, *args, **kwargs ):
+        self.logHeader()
+        traceback.print_stack( file=sys.stderr )
+        self.doPrint( *args, **kwargs )
+        
+    def DoLogTextAtLevel( self, *args, **kwargs ):
+        self.logHeader()
+        traceback.print_stack( file=sys.stderr )
+        self.doPrint( *args, **kwargs )
+        
 if __name__ == '__main__':
-	app = wx.App( False )
-	wx.Log.SetActiveTarget( LogPrintStackStderr() )
-	bitmap = wx.Bitmap( 'NonExistentFile.png' )
+    app = wx.App( False )
+    wx.Log.SetActiveTarget( LogPrintStackStderr() )
+    bitmap = wx.Bitmap( 'NonExistentFile.png' )
