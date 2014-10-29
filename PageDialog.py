97dd3d3a5a6a26e10a2df4961cf2e2ecda0f5164
Edward Sitarski <edward.sitarski@gmail.com>
2014-10-28 21:06:18 -0400

    Added minimize and maximize buttons, also added dialog icons.

--- PageDialog.py
+++ PageDialog.py
@@ -1,16 +1,21 @@
 import wx
+import os
 import Utils
 
 class PageDialog( wx.Dialog ):
 	def __init__(
 			self, parent, pageClass, closeCallback=None, title=_("Change Properties"),
 			ID = wx.ID_ANY, size=wx.DefaultSize, pos=wx.DefaultPosition, 
-			style = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER,
+			style = wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER|wx.MINIMIZE_BOX|wx.MAXIMIZE_BOX,
 		):
 
 		if size == wx.DefaultSize:
 			size = tuple( int(v*0.5) for v in wx.GetDisplaySize() )
 		super( PageDialog, self ).__init__( parent, ID, title=title, size=size, pos=pos, style=style )
+		
+		# Set the upper left icon.
+		icon = wx.Icon( os.path.join(Utils.getImageFolder(), 'CrossMgr16x16.ico'), wx.BITMAP_TYPE_ICO )
+		self.SetIcon( icon )
 		
 		sizer = wx.BoxSizer( wx.VERTICAL )
 		self.SetSizer(sizer)
