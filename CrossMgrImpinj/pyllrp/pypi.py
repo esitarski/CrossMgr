9caa5bbc1ef3fb429c72aff00569c4ef2cb82aa3
Edward Sitarski <edward.sitarski@gmail.com>
2015-04-02 22:37:58 -0400

    Fixed pypi.py installation explanation.

--- CrossMgrImpinj/pyllrp/pypi.py
+++ CrossMgrImpinj/pyllrp/pypi.py
@@ -62,7 +62,7 @@ All Message and Parameters are full Python classes.
 Full validation of all LLRP Messages and Parameters including data type, date values and parameter checking.
 Full support for enumerated values.
 
-See TinyExample.py and wxExample.py for how to use.
+See TinyExample.py for how to use.
 
 A reader connection manager is also included that can connect to a reader, transact commands, then start/stop a thread to listen for tag reads.  A message handler can be configured to respond to any reader message.  See wxExample.py for a simple method to show reader messages in a wxPython application with a Queue (requires wxPython install).
 
