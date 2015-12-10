0b6d1932081b1967ec6b54d6a4f971c3814ce5aa
Edward Sitarski <edward.sitarski@gmail.com>
2015-12-10 09:52:57 -0500

    Fixed another bug in the chip reader update.

--- RaceInputState.py
+++ RaceInputState.py
@@ -8,11 +8,28 @@ class RaceInputState( object ):
 		self.reset()
 	
 	def reset( self ):
-		self.state = (None, None, None)
+		self.state = None
 	
 	def changed( self ):
-		newState = (Utils.getFileName(), ReadSignOnSheet.stateCache, getattr(Model.race, 'lastOpened', None) if Model.race else None)
-		if self.state != newState:
+		race = Model.race
+		newState = [
+			Utils.getFileName(),
+			ReadSignOnSheet.stateCache,
+		]
+		if race:
+			newState.extend( [
+					getattr(race, 'lastOpened', None),
+					race.enableJChipIntegration,
+					race.resetStartClockOnFirstTag,
+					race.firstRecordedTime,
+					race.skipFirstTagRead ,
+					
+					race.chipReaderType,
+					race.chipReaderPort,
+					race.chipReaderIpAddr,
+				]
+			)
+		if not self.state or self.state != newState:
 			self.state = newState
 			return True
 		return False
