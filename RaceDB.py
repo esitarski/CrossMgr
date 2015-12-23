54df3705abc3fbf1a1ff6976e1c17235d9b4af9c
Edward Sitarski <edward.sitarski@gmail.com>
2015-12-23 10:26:45 -0500

    Improved RaceDB interface.

--- RaceDB.py
+++ RaceDB.py
@@ -6,21 +6,22 @@ import wx.gizmos as gizmos
 import Utils
 import Clock
 
-UrlRaceDB = 'http://127.0.0.1:8000/RaceDB'
+RaceDBUrlDefault = 'http://127.0.0.1:8000/RaceDB'
 
 def GetRaceDBEvents( date=None ):
-	url = UrlRaceDB + '/GetEvents'
+	url = RaceDBUrlDefault + '/GetEvents'
 	if date:
 		url += date.strftime('/%Y-%m-%d')
 	req = requests.get( url + '/' )
-	print req.content
 	events = req.json()
 	return events
 	
 def GetEventCrossMgr( eventId, eventType ):
-	url = UrlRaceDB + ['/EventMassStartCrossMgr','/EventTTCrossMgr'][eventType] + '/{}'.format(eventId)
+	url = RaceDBUrlDefault + ['/EventMassStartCrossMgr','/EventTTCrossMgr'][eventType] + '/{}'.format(eventId)
 	req = requests.get( url + '/' )
-	return req.content
+	content_disposition = req.headers['content-disposition'].encode('latin-1').decode('utf-8')
+	filename = content_disposition.split('=')[1].replace("'",'').replace('"','')
+	return filename, req.content
 
 class RaceDB( wx.Dialog ):
 	def __init__( self, parent, id=wx.ID_ANY, size=(600,900) ):
@@ -62,19 +63,24 @@ class RaceDB( wx.Dialog ):
 		self.tree.SetImageList( self.il )
 		
 		self.tree.AddColumn( _('Event Info') )
+		self.tree.AddColumn( _('Event Type'), flag=wx.ALIGN_LEFT )
+		self.eventTypeCol = 1
 		self.tree.AddColumn( _('Start Time'), flag=wx.ALIGN_RIGHT )
-		self.startTimeCol = 1
+		self.startTimeCol = 2
 		self.tree.AddColumn( _('Participants'), flag=wx.ALIGN_RIGHT)
-		self.participantCountCol = 2
+		self.participantCountCol = 3
 		
 		self.tree.SetMainColumn( 0 )
 		self.tree.SetColumnWidth( 0, 320 )
+		self.tree.SetColumnWidth( self.eventTypeCol, 80 )
+		self.tree.SetColumnWidth( self.startTimeCol, 80 )
+		self.tree.SetColumnWidth( self.participantCountCol, 80 )
 		
 		self.tree.Bind( wx.EVT_TREE_SEL_CHANGED, self.selectChangedCB )
-		self.pkSelect = None
+		self.dataSelect = None
 		
 		hs = wx.BoxSizer( wx.HORIZONTAL )
-		self.okButton = wx.Button( self, label=_("Initialize Timing for this Event") )
+		self.okButton = wx.Button( self, label=_("Initialize Timing for Event") )
 		self.okButton.Bind( wx.EVT_BUTTON, self.doOK )
 		self.cancelButton = wx.Button( self, id=wx.ID_CANCEL )
 		hs.Add( self.okButton )
@@ -88,13 +94,24 @@ class RaceDB( wx.Dialog ):
 		self.SetSizer( vs )
 
 	def doOK( self, event ):
-		pass
+		url = self.raceDBUrl.GetValue().strip()
+		if not url:
+			url = RaceDBUrlDefault
+		dir = self.raceFolder.GetPath().strip()
+		if not dir:
+			dir = os.path.expanduser('~')
+		try:
+			filename, content = GetEventCrossMgr( self.dataSelect['pk'], self.dataSelect['event_type'] )
+		except Exception as e:
+			raise e
+		print filename
+		self.EndModal( wx.ID_OK )
 		
 	def selectChangedCB( self, evt ):
 		try:
-			self.pkSelect = self.tree.GetItemPyData(evt.GetItem())['pk']
+			self.dataSelect = self.tree.GetItemPyData(evt.GetItem())
 		except Exception as e:
-			self.pkSelect = None
+			self.dataSelect = None
 		
 	def refresh( self, events ):
 		competitions = {}
@@ -123,30 +140,32 @@ class RaceDB( wx.Dialog ):
 			
 		tNow = datetime.datetime.now()
 		eventClosest = None
-		self.pkSelect = None
+		self.dataSelect = None
 		
 		for cName, events, participant_count, num in sorted(
 				((c['name'], c['events'], c['participant_count'], c['num']) for c in competitions.itervalues()), key=lambda x: x[-1] ):
 			competition = self.tree.AppendItem( self.root, cName )
 			self.tree.SetItemText( competition, unicode(participant_count), self.participantCountCol )
 			for e in events:
-				event = self.tree.AppendItem( competition, u'{}: {}'.format(_('Event'), e['name']), data=wx.TreeItemData({'pk':e['pk']}) )
+				eventData = {'pk':e['pk'], 'event_type':e['event_type']}
+				event = self.tree.AppendItem( competition, u'{}: {}'.format(_('Event'), e['name']), data=wx.TreeItemData(eventData) )
 				self.tree.SetItemText( event, get_tod(e['date_time']), self.startTimeCol )
+				self.tree.SetItemText( event, _('Mass Start') if e['event_type'] == 0 else _('Time Trial'), self.eventTypeCol )
 				self.tree.SetItemText( event, unicode(e['participant_count']), self.participantCountCol )
 				
 				tEvent = datetime.datetime.combine( tNow.date(), get_time(e['date_time']) )
 				if eventClosest is None and tEvent > tNow:
 					eventClosest = event
-					self.pkSelect = e['pk']
+					self.dataSelect = e['pk']
 				
 				for w in e['waves']:
-					wave = self.tree.AppendItem( event, u'{}: {}'.format(_('Wave'), w['name']), data=wx.TreeItemData({'pk':e['pk']}) )
+					wave = self.tree.AppendItem( event, u'{}: {}'.format(_('Wave'), w['name']), data=wx.TreeItemData(eventData) )
 					self.tree.SetItemText( wave, unicode(w['participant_count']), self.participantCountCol )
 					start_offset = w.get('start_offset',None)
 					if start_offset:
 						self.tree.SetItemText( wave, '+' + start_offset, self.startTimeCol )
 					for cat in w['categories']:
-						category = self.tree.AppendItem( wave, cat['name'], data=wx.TreeItemData({'pk':e['pk']}) )
+						category = self.tree.AppendItem( wave, cat['name'], data=wx.TreeItemData(eventData) )
 						self.tree.SetItemText( category, unicode(cat['participant_count']), self.participantCountCol )
 			self.tree.Expand( competition )
 						
