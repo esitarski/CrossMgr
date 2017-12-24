772486f37a9d90a82a82228079c0791dc26a9b9f
Mark Buckaway <mark@buckaway.ca>
2017-12-23 22:01:05 -0500

    Added dependancy.py for building from source and linuxdeps.sh for ubuntu linux

---
+++ dependencies.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+import os
+import sys
+import shutil
+import argparse
+import subprocess
+import compileall
+import platform
+
+is_windows = (platform.system() == 'Windows')
+
+dependencies = [
+	'requests',
+	'waitress',
+	'xlsxwriter',
+	'pygments',
+	'xlrd',
+	'pytz',
+	'fpdf',
+	'natural-keys',
+	'xlwt',
+	'netifaces',
+	'whoosh',
+	'qrcode',
+	'tornado',
+	'ftputil'
+]
+
+uninstall_dependencies = [
+	'south',
+]
+
+def update_dependencies( upgrade ):
+	print( 'Updating Dependencies...' )
+	
+	pip = 'C:/Python27/Scripts/pip.exe'
+	if os.path.isfile(pip):
+		print( 'Found "pip" at "{}".'.format(pip) )
+	else:
+		pip = 'pip'
+	
+	for d in dependencies:
+		args = [pip, 'install', d]
+		if upgrade:
+			args.append('--upgrade')
+		print( ' '.join(args) )
+		subprocess.call( args )
+
+	for d in uninstall_dependencies:
+		args = [pip, 'uninstall', d]
+		print( ' '.join(args) )
+		subprocess.call( args )
+
+	print( 'Removing old compiled files...' )
+	for root, dirs, files in os.walk( '.' ):
+		for f in files:
+			fname = os.path.join( root, f )
+			if os.path.splitext(fname)[1] == '.pyc':
+				os.remove( fname )
+	
+	print( 'Pre-compiling source code...' )
+	compileall.compile_dir( '.', quiet=True )
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser( description='Update CrossMgr Dependencies' )
+	parser.add_argument(
+		'--upgrade',
+		action='store_true',
+		default=False,
+	)
+	
+	args = parser.parse_args()
+	update_dependencies( args.upgrade )
