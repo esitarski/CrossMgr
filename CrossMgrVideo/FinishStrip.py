e0c54aed951eeb07c2a6d6b4cd7f73561bf8279f
Edward Sitarski <edward.sitarski@gmail.com>
2018-02-27 09:07:41 -0500

    Removed dead code.

--- CrossMgrVideo/FinishStrip.py
+++ CrossMgrVideo/FinishStrip.py
@@ -249,13 +249,6 @@ class FinishStrip( wx.Panel ):
 		else:
 			xViewPos, yViewPos = winWidth - viewWidth + penWidthDiv2, penWidthDiv2
 
-		'''
-		if xViewPos <= x < xViewPos + viewWidth:
-			if self.leftToRight:
-				xViewPos, yViewPos = penWidthDiv2, penWidthDiv2
-			else:
-				xViewPos, yViewPos = winWidth - viewWidth + penWidthDiv2, penWidthDiv2
-		'''
 		if xViewPos <= x < xViewPos + viewWidth:
 			if self.leftToRight:
 				xViewPos, yViewPos = penWidthDiv2, penWidthDiv2
