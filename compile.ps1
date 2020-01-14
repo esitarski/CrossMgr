6c8b7737a6eef4cb8203f0af7d0edf92a3482c8d
Mark Buckaway <mark@buckaway.ca>
2020-01-13 20:36:46 -0500

    windows changes

--- compile.ps1
+++ compile.ps1
@@ -564,7 +564,7 @@ function DoRelease
 	$tagname = "v$versionno-$date"
 	Write-Host "Tagging with $tagname"
 	Start-Process -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList "checkout master"
-	Start-Process -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList "merge dev -m 'Release $tagname'"
+	Start-Process -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList "merge dev -m `"Release $tagname`""
 	Start-Process -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList "push"
 	Start-Process -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList "tag $tagname"
 	Start-Process -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList "push origin $tagname"
