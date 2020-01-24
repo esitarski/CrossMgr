a4c906d2cc3ffd5c8098ca69889c0c64cfb8da73
Mark Buckaway <mark@buckaway.ca>
2020-01-24 13:13:13 -0500

    Fixed linux netifaces dep.

--- linuxdeps.sh
+++ linuxdeps.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Ubuntu dependancies required for wxPython 4.0.0rc1
 sudo apt update
-sudo apt install libjpeg9 libtiff5 libsdl2-2.0-0 libnotify-bin freeglut3 libsm6 libgtk-3-0 libwebkitgtk-3.0-0 libgstreamer1.0-0 libgtk-3-dev
+sudo apt install libjpeg9 libtiff5 libsdl2-2.0-0 libnotify-bin freeglut3 libsm6 libgtk-3-0 libwebkitgtk-3.0-0 libgstreamer1.0-0 libgtk-3-dev python3.7-dev
