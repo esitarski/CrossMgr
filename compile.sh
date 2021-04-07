8a3f4e965b7434b75a7e6f66ffa03bef6e6447d9
Edward Sitarski <edward.sitarski@gmail.com>
2021-04-07 17:58:29 -0400

    Added verbose option to pip.

--- compile.sh
+++ compile.sh
@@ -289,9 +289,9 @@ envSetup() {
         # The build also takes >40 minutes, which is an excessive amount of time to wait for a failed build.
         # The solution is to grab the pre-built install for this Ubuntu version from wxPython extras.
 		UBUNTU_RELEASE=`lsb_release -r | awk '{ print $2 }'`
-		sed "s+wxPython+-f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-${UBUNTU_RELEASE} wxPython+g" < requirements.txt | pip3 install -r /dev/stdin
+		sed "s+wxPython+-f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-${UBUNTU_RELEASE} wxPython+g" < requirements.txt | pip3 install -v -r /dev/stdin
 	else
-		pip3 install -r requirements.txt
+		pip3 install -v -r requirements.txt
 	fi
     if [ $? -ne 0 ];then
         echo "Pip requirements install failed. Aborting..."
