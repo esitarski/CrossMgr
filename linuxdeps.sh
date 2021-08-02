c01795a1caf56b1cc82a3d4f63345e1f2e74b363
esitarski <edward.sitarski@gmail.com>
2021-08-02 09:42:28 -0400

    Removed missing libraries for linux build.

--- linuxdeps.sh
+++ linuxdeps.sh
@@ -5,4 +5,4 @@ sudo apt update
 # sudo apt install libjpeg9 libtiff5 libsdl2-2.0-0 libnotify-bin freeglut3 libsm6 libgtk-3-0 libwebkitgtk-3.0-0 libgstreamer1.0-0 libgtk-3-dev python3.9-dev
 
 # Some libraries are still required for wxPython.
-sudo apt install libsdl2-2.0-0 libgtk-3-0 libwebkitgtk-3.0-0 libgstreamer1.0-0 libgtk-3-dev
+sudo apt install libsdl2-2.0-0 libgtk-3-0 libgtk-3-dev
