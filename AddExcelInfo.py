1a4d6bc975363084ba30d1649fd521c8d4b9013f
esitarski <edward.sitarski@gmail.com>
2021-09-15 08:25:20 -0400

    Fixed update bug.

--- AddExcelInfo.py
+++ AddExcelInfo.py
@@ -1,23 +1,26 @@
 import os
-import pwd
 import sys
 import datetime
 import platform
 import Version
 
-def get_user():
-	try:
-		return pwd.getpwuid( os.getuid() )[0]
-	except Exception:
-		return 'unknown'
+def getInfo():
+	app = Version.AppVerName.split()[0]
+	uname = platform.uname()
+	info = {
+		'{}_AppVersion'.format(app):	Version.AppVerName,
+		'{}_Timestamp'.format(app):		datetime.datetime.now(),
+		'{}_User'.format(app):			os.getlogin(),
+		'{}_Python'.format(app):		sys.version.replace('\n', ' '),
+	}
+	info.update( {'{}_{}'.format(app, a.capitalize()): getattr(uname, a, '')
+		for a in ('system', 'release', 'version', 'machine', 'processor') } )
+	return info
 
 def AddExcelInfo( wb ):
-	app = Version.AppVerName.split[0]
-	uname = platform.uname()
-	set_custom_property = wb.set_custom_property
-	set_custom_property('{}_AppVersion'.format(app),	Version.AppVerName)
-	set_custom_property('{}_Timestamp'.format(app),		datetime.datetime.now() )
-	set_custom_property('{}_User.format(app)',			get_user())
-	set_custom_property('{}_Python.format(app)',		sys.version.replace('\n', ' '))
-	for a in ('system', 'release', 'version', 'machine', 'processor'):
-		set_custom_property( '{}_{}'.format(app, a.capitalize()), getattr(uname, a, ''))
+	for k,v in getInfo().items():
+		wb.set_custom_property( k, v )
+
+if __name__ == '__main__':
+	for k, v in getInfo().items():
+		print( k, v )
