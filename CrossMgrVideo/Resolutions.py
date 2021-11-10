0b03d8c7b0d1e5518512246b7bc7dd9ea34ccee8
esitarski <edward.sitarski@gmail.com>
2021-11-10 12:49:47 -0500

    Added 1M, 2M names to camera resolutions.

--- CrossMgrVideo/Resolutions.py
+++ CrossMgrVideo/Resolutions.py
@@ -9,21 +9,26 @@ WGA	800×480	0.384	Unknown
 SVGA	800×600	0.480	Unknown
 DVCPRO HD	960×720	0.691	Unknown
 XGA	1024×768	0.786	Unknown
-HD	1280×720	0.922	Unknown
+1MP/720P	1280×720	0.922	Unknown
 WXGA	1280×800	1.024	Unknown
-SXGA−	1280×960	1.229	Unknown
+960P/SXGA−	1280×960	1.229	Unknown
 SXGA	1280×1024	1.311	Unknown
 UXGA	1600×1200	1.920	Unknown
-FHD	1920×1080	2.074	Unknown
-QXGA	2048×1536	3.146	Unknown
+2MP/1080P/FHD	1920×1080	2.074	Unknown
+3MP/QXGA	2048×1536	3.146	Unknown
+4MP	2592×1520	5.243	Unknown
+5MP	2560×1060	5.243	Unknown
 QSXGA	2560×2048	5.243	Unknown
+6MP	3072×2048	7.680	Unknown
 QUXGA	3200×2400	7.680	Unknown
+4K Ultra HD	3840×2160	7.680	Unknown
 DCI 4K	4096×2160	8.847	Unknown
 HXGA	4096×3072	12.583	Unknown
 UW5K	5120×2160	11.059	Unknown
 5K	5120×2880	14.746	Unknown
 WHXGA	5120×3200	16.384	Unknown
 HSXGA	5120×4096	20.972	Unknown
+8K Ultra HD	7680×4320	20.972	Unknown
 '''
 
 resolutions = ['{1} [{0}]'.format(*line.split('\t')[:2]) for line in resolutions.split('\n') if line][1:]
