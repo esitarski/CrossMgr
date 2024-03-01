947d5b3eb289a59ff35f89f4f66afe4217dc0e50
esitarski <edward.sitarski@gmail.com>
2024-02-29 20:53:20 -0500

    Added utf-8 encoding to files opened in UpdateDependencies.py

--- SprintMgr/UpdateDependencies.py
+++ SprintMgr/UpdateDependencies.py
@@ -15,11 +15,11 @@ def UpdateDependencies():
 			except Exception:
 				pass
 			
-			with open(os.path.join('..', fname)) as fc:
+			with open(os.path.join('..', fname), encoding='utf-8') as fc:
 				contents = fc.read()
 			if fname in ('Utils.py', 'HelpIndex.py'):
 				contents = contents.replace("'CrossMgr", "'SeriesMgr")
-			with open(fname, 'w') as fc:
+			with open(fname, 'w', encoding='utf-8') as fc:
 				fc.write( contents )
 			
 if __name__ == '__main__':
