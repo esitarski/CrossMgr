fa76dd6bf7186c0c7d4bbcb012e1ac7ccf5ad3cf
esitarski <edward.sitarski@gmail.com>
2024-04-20 08:41:35 -0400

    Added more utf8 encoding.

--- helptxt/compile.py
+++ helptxt/compile.py
@@ -50,18 +50,18 @@ def CompileHelp( dir = '.' ):
 				output_format='html5'
 		)
 
-		with open('markdown.css', 'r') as f:
+		with open('markdown.css', 'r', encoding='utf8') as f:
 			style = f.read()
-		with open('prolog.html', 'r') as f:
+		with open('prolog.html', 'r', encoding='utf8') as f:
 			prolog = f.read()
 			prolog = prolog.replace( '<<<style>>>', style, 1 )
 			del style
-		with open('epilog.html', 'r') as f:
+		with open('epilog.html', 'r', encoding='utf8') as f:
 			epilog = f.read().replace('YYYY','{}'.format(datetime.datetime.now().year))
 
 		contentDiv = '<div class="content">'
 		
-		with open('Links.md', 'r') as f:
+		with open('Links.md', 'r', encoding='utf8') as f:
 			links = f.read()
 			
 		for fname in getHelpFiles():
